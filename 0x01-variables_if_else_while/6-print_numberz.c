79bc47ea25dcb2d62aaaa8060963631d0b951096
IanOkoth2 <okothian36@gmail.com>
2022-07-02 06:20:34 -0700

    Printing numbers from 1 to 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: A program that prints all single digit numbers.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
