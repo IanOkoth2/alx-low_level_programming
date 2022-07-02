71756857ddb369367b8de2c128ad9684e30d57b9
IanOkoth2 <okothian36@gmail.com>
2022-07-02 06:40:24 -0700

    Printing all base 16 digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Printing all the base 16 numbers.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int c;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+
+        for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
