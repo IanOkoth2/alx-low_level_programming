f107b7cad207b8a9f496e3aed1f3ffb90eae728c
IanOkoth2 <okothian36@gmail.com>
2022-07-02 04:51:49 -0700

    Printing the last digit of a number in comparison to 5

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+        int m; 
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	m = n % 10;
+
+	if (m > 5) 
+		printf("The last digit of %i is %i and is greater than 5\n", n, m);
+	else if (m == 0) 
+		printf("The last digit of %i is %i and is 0\n", n ,m);
+	else 
+		printf("The last digit of %i is %i and is less than 6 and not 0\n", n, m);
+
+	return (0);
+}
